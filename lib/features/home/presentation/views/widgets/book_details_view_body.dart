6e0d4e547ef280ca8e63389804ce9c949fc78e15
mo <mohammedali200028@gmail.com>
2023-09-17 21:38:26 +0300

    Add custom image to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'custom_book_item.dart';
 import 'custom_detiles_app_bar.dart';
-import 'custom_list_view_item.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
    const BookDetailsViewBody({Key? key}) : super(key: key);
--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'custom_list_view_item.dart';
+import 'custom_book_item.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({Key? key}) : super(key: key);
